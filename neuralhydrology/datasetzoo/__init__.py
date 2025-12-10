3c7fd1035e8acb6beaf8b7d8ec62961c78127a22
gjohnen1 <gregor.johnen@posteo.de>
2025-12-10 16:23:00 +0100

    Register OnlineForecastDataset in the dataset registry

--- neuralhydrology/datasetzoo/__init__.py
+++ neuralhydrology/datasetzoo/__init__.py
@@ -12,6 +12,7 @@ from neuralhydrology.datasetzoo.camelsind import CamelsIND
 from neuralhydrology.datasetzoo.genericdataset import GenericDataset
 from neuralhydrology.datasetzoo.hourlycamelsus import HourlyCamelsUS
 from neuralhydrology.datasetzoo.lamah import LamaH
+from neuralhydrology.datasetzoo.onlineforecastdataset import OnlineForecastDataset
 from neuralhydrology.utils.config import Config
 from neuralhydrology.datasetzoo.datasetregistry import DatasetRegistry
 
@@ -112,4 +113,5 @@ _datasetZooRegistry.register_dataset_class("lamah_c", LamaH)
 _datasetZooRegistry.register_dataset_class("caravan", Caravan)
 _datasetZooRegistry.register_dataset_class("camels_ind", CamelsIND)
 _datasetZooRegistry.register_dataset_class("camels_de", CamelsDE)
+_datasetZooRegistry.register_dataset_class("online_forecast", OnlineForecastDataset)
 
